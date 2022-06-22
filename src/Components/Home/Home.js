a9fc4d3f93a1d2bff933c4fa91b1b1d0f22d85a6
Aniksaha-ui <sahaanik1045@gmail.com>
2022-06-22 19:03:01 +0600

    Basic project structure done

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container-fluid mt-5">
+      <div className="row border-2s">
+        <div className="col-lg-4 col-md-4 col-12">SideBar</div>
+        <div className="col-lg-8 col-md-8 col-12">Main Part</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
