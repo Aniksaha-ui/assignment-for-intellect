4cbd684bc0e7f7a231c388332171da0abeac5076
Aniksaha-ui <sahaanik1045@gmail.com>
2022-06-24 13:15:08 +0600

    project done

--- src/Components/Home/Banner.js
+++ src/Components/Home/Banner.js
@@ -2,8 +2,19 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className="card">
-      <h3>Hello World</h3>
+    <div className="card bg-custom p-2">
+      <div className="d-flex align-items-center justify-content-center">
+        <h3>
+          Welcome
+          <br></br> <span className="text-primary">Pablo Nicolus</span>
+        </h3>
+        <img
+          className="img-fluid ms-5"
+          width="350"
+          src="https://dashboardbuilder.net/images/main-banner-new-min.png"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
--- src/Components/Home/Sidebar.js
+++ src/Components/Home/Sidebar.js
@@ -19,53 +19,59 @@ const Sidebar = () => {
           <Link to="/" className="nav-item text-dark">
             <i class="fa-solid fa-chart-column fa-xl"></i>
           </Link>
-          <p className="ms-2">Dashboard</p>
+          <p className="ms-2">Chart</p>
         </div>
         <div className="d-flex ms-3 mt-1">
           <Link to="/" className="nav-item text-dark">
             <i class="fa-solid fa-file fa-xl"></i>
           </Link>
-          <p className="ms-2">Dashboard</p>
+          <p className="ms-2">Page Layout</p>
         </div>
       </div>
       <div className="card  p-3 rounded-20 mt-4">
         <div className="d-flex ms-3">
           <Link to="/" className="nav-item text-dark">
-            <i class="fa-solid fa-bars fa-xl"></i>
+            <i class="fa-solid fa-inbox fa-xl"></i>
           </Link>
-          <p className="ms-2">Dashboard</p>
+          <p className="ms-2">inbox</p>
         </div>
         <div className="d-flex ms-3 mt-1">
           <Link to="/" className="nav-item text-dark">
-            <i class="fa-solid fa-chart-column fa-xl"></i>
+            <i class="fa-solid fa-ticket fa-xl"></i>
           </Link>
-          <p className="ms-2">Dashboard</p>
+          <p className="ms-2">Ticket</p>
         </div>
         <div className="d-flex ms-3 mt-1">
           <Link to="/" className="nav-item text-dark">
-            <i class="fa-solid fa-file fa-xl"></i>
+            <i class="fa-solid fa-calendar-days fa-xl"></i>
           </Link>
-          <p className="ms-2">Dashboard</p>
+          <p className="ms-2">Extra</p>
         </div>
       </div>
       <div className="card p-3 rounded-20 mt-4 mb-4">
         <div className="d-flex ms-3">
           <Link to="/" className="nav-item text-dark">
-            <i class="fa-solid fa-bars fa-xl"></i>
+            <i class="fa-brands fa-uikit fa-xl"></i>
           </Link>
-          <p className="ms-2">Dashboard</p>
+          <p className="ms-2">UI Element</p>
         </div>
         <div className="d-flex ms-3 mt-1">
           <Link to="/" className="nav-item text-dark">
-            <i class="fa-solid fa-chart-column fa-xl"></i>
+            <i class="fa-solid fa-address-card fa-xl"></i>
           </Link>
-          <p className="ms-2">Dashboard</p>
+          <p className="ms-2">Cards</p>
         </div>
         <div className="d-flex ms-3 mt-1">
           <Link to="/" className="nav-item text-dark">
-            <i class="fa-solid fa-file fa-xl"></i>
+            <i class="fa-solid fa-clipboard fa-xl"></i>
           </Link>
-          <p className="ms-2">Dashboard</p>
+          <p className="ms-2">Component</p>
+        </div>
+        <div className="d-flex ms-3 mt-1">
+          <Link to="/" className="nav-item text-dark">
+            <i class="fa-solid fa-table-cells-large"></i>
+          </Link>
+          <p className="ms-2">Widget</p>
         </div>
       </div>
     </div>
